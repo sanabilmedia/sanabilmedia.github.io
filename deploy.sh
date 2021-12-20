cb786c3a75e7b1723e9bb2aed043a24fcc4fc727
Abdou <abd.pers@gmail.com>
2021-12-20 22:07:17 +0100

    uba commit

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ cd ../sanabilmedia.github.io
 git add .
 git commit -m "uba commit"
 git push origin main
-printf "\e[32m\nSuccessfully deployed the website!\e[39m"
+printf "\e[32m\nSuccessfully deployed the website!\e[39m\n\n"
 
